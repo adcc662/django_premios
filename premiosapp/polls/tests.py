a6e95ec9e1b8378dcb899c3e67b211ffa68f6e70
Angel David Castillo Castro <adcc662@gmail.com>
2022-03-22 12:26:42 -0600

    Test about questions in the past and future

--- premiosapp/polls/tests.py
+++ premiosapp/polls/tests.py
@@ -10,9 +10,20 @@ class QuestionModelTests(TestCase):
         """Was published recently returns false for questions whose
             pub_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="¿Quien es el mejor Course Director de Platzi?", pub_date=time)
+        future_question = Question(question_text="¿Quien es el mejor CD de platzi?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-
+    
+    def test_was_published_recently_with_past_questions(self):
+        """Was published recently returns false for questions whose pub_date is in the past"""
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text="¿Quien es el mejor CD de platzi?", pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_present_questions(self):
+        """Was published recently returns false for questions whose pub_date is in the present"""
+        time = timezone.now()
+        present_question = Question(question_text="¿Quien es el mejor CD de platzi?", pub_date=time)
+        self.assertIs(present_question.was_published_recently, True)
 
 
 
